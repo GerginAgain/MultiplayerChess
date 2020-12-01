991ab084891430ec60f06ce6dee749b4560eb311
GerginAgain <GerginAgain@gmail.com>
2020-12-01 11:47:34 +0200

    Add IDeletableEntity interface to Chess.Data.Common

---
+++ Data/Chess.Data.Common/IDeletableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.Data.Common
+{
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
