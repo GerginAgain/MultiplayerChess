9f040dd9ec9a43b4eec1c417810f6c899357d426
GerginAgain <GerginAgain@gmail.com>
2020-12-02 12:41:56 +0200

    Add automapping project

---
+++ Web/Chess.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Chess.Common;
+
+namespace Chess.Web.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class UsersController : Controller
+    {
+        //public async Task<IActionResult> Active(int? pageNumber)
+        //{
+        //    var allUserViewModels = await usersService.GetAllUserViewModelsAsync(pageNumber ?? GlobalConstants.DefaultPageNumber, GlobalConstants.DefaultPageSize);
+
+        //    return View(allUserViewModels);
+        //}
+    }
+}
