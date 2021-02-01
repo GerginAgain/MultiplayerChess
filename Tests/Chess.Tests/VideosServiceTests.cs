8532e637636e0517663e1e1a63da5d4cdcab2b35
GerginAgain <GerginAgain@gmail.com>
2021-02-01 23:30:50 +0200

    Add 4 tests for method 'GetAllActiveVideosViewModelsAsync' in 'VideosService'

--- Tests/Chess.Tests/VideosServiceTests.cs
+++ Tests/Chess.Tests/VideosServiceTests.cs
@@ -280,5 +280,131 @@
             Assert.Equal(expectedResult, actual.Count);
 
         }
+
+        [Fact]
+        public async Task GetAllActiveVideosViewModelsAsync_WithoutAnyVideos_ShouldReturnZero()
+        {
+            //Arrange
+            var expectedResult = 0;
+
+            var moqPictureService = new Mock<IPicturesService>();
+            var moqUsersService = new Mock<IUsersService>();
+
+            var option = new DbContextOptionsBuilder<ChessDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var db = new ChessDbContext(option);
+
+            this.videosService = new VideosService(db, moqPictureService.Object, mapper, moqUsersService.Object);
+
+            //Act
+            var actual = await this.videosService.GetAllActiveVideosViewModelsAsync(1, 10);
+
+            //Assert
+            Assert.Equal(expectedResult, actual.Count);
+        }
+
+        [Fact]
+        public async Task GetAllActiveVideosViewModelsAsync_WithoutAnyDeletedVideos_ShouldReturnCorrectCount()
+        {
+            //Arrange
+            var expectedResult = 5;
+
+            var moqPictureService = new Mock<IPicturesService>();
+            var moqUsersService = new Mock<IUsersService>();
+
+            var option = new DbContextOptionsBuilder<ChessDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var db = new ChessDbContext(option);
+
+            this.videosService = new VideosService(db, moqPictureService.Object, mapper, moqUsersService.Object);
+
+            var testingVideos = new List<Video>
+            {
+                new Video{Id = 1, Title = "Video1", IsDeleted = false, Link = "link1", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" } },
+                new Video{Id = 2, Title = "Video2", IsDeleted = false, Link = "link2", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 3, Title = "Video3", IsDeleted = false, Link = "link3", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 4, Title = "Video4", IsDeleted = false, Link = "link4", CreatedOn = DateTime.UtcNow.AddDays(-20), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 5, Title = "Video5", IsDeleted = false, Link = "link5", CreatedOn = DateTime.UtcNow.AddDays(-5), Picture = new Picture{Link = "PictureLink" }},
+            };
+
+            await db.Videos.AddRangeAsync(testingVideos);
+            await db.SaveChangesAsync();
+
+            //Act
+            var actual = await this.videosService.GetAllActiveVideosViewModelsAsync(1, 10);
+
+            //Assert
+            Assert.Equal(expectedResult, actual.Count);
+        }
+
+        [Fact]
+        public async Task GetAllActiveVideosViewModelsAsync_WithDeletedVideos_ShouldReturnCorrectCount()
+        {
+            //Arrange
+            var expectedResult = 3;
+
+            var moqPictureService = new Mock<IPicturesService>();
+            var moqUsersService = new Mock<IUsersService>();
+
+            var option = new DbContextOptionsBuilder<ChessDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var db = new ChessDbContext(option);
+
+            this.videosService = new VideosService(db, moqPictureService.Object, mapper, moqUsersService.Object);
+
+            var testingVideos = new List<Video>
+            {
+                new Video{Id = 1, Title = "Video1", IsDeleted = true, Link = "link1", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" } },
+                new Video{Id = 2, Title = "Video2", IsDeleted = false, Link = "link2", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 3, Title = "Video3", IsDeleted = true, Link = "link3", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 4, Title = "Video4", IsDeleted = false, Link = "link4", CreatedOn = DateTime.UtcNow.AddDays(-20), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 5, Title = "Video5", IsDeleted = false, Link = "link5", CreatedOn = DateTime.UtcNow.AddDays(-5), Picture = new Picture{Link = "PictureLink" }},
+            };
+
+            await db.Videos.AddRangeAsync(testingVideos);
+            await db.SaveChangesAsync();
+
+            //Act
+            var actual = await this.videosService.GetAllActiveVideosViewModelsAsync(1, 10);
+
+            //Assert
+            Assert.Equal(expectedResult, actual.Count);
+        }
+
+        [Fact]
+        public async Task GetAllActiveVideosViewModelsAsync_WithValidData_ShouldReturnCorrectOrder()
+        {
+            //Arrange
+            var expectedResult = new List<int> { 5, 4, 2 }; ;
+
+            var moqPictureService = new Mock<IPicturesService>();
+            var moqUsersService = new Mock<IUsersService>();
+
+            var option = new DbContextOptionsBuilder<ChessDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var db = new ChessDbContext(option);
+
+            this.videosService = new VideosService(db, moqPictureService.Object, mapper, moqUsersService.Object);
+
+            var testingVideos = new List<Video>
+            {
+                new Video{Id = 1, Title = "Video1", IsDeleted = true, Link = "link1", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" } },
+                new Video{Id = 2, Title = "Video2", IsDeleted = false, Link = "link2", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 3, Title = "Video3", IsDeleted = true, Link = "link3", CreatedOn = DateTime.UtcNow.AddDays(-25), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 4, Title = "Video4", IsDeleted = false, Link = "link4", CreatedOn = DateTime.UtcNow.AddDays(-20), Picture = new Picture{Link = "PictureLink" }},
+                new Video{Id = 5, Title = "Video5", IsDeleted = false, Link = "link5", CreatedOn = DateTime.UtcNow.AddDays(-5), Picture = new Picture{Link = "PictureLink" }},
+            };
+
+            await db.Videos.AddRangeAsync(testingVideos);
+            await db.SaveChangesAsync();
+
+            //Act
+            var actual = this.videosService.GetAllActiveVideosViewModelsAsync(1, 10).GetAwaiter().GetResult().ToList();
+
+            //Assert
+            Assert.Equal(expectedResult[0], actual[0].Id);
+            Assert.Equal(expectedResult[1], actual[1].Id);
+            Assert.Equal(expectedResult[2], actual[2].Id);
+        }
     }
 }
